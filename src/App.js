6950303d222f8196c2eb992ac8ec597af2d295c2
Humza Aamir <144220544+Humza911@users.noreply.github.com>
2024-01-12 11:23:55 +1100

    Added Scrollbar to the Monsters List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import { robots } from './robots';
+//import { robots } from './robots';
 import CardsList from './CardsList';
 import SearchBox from './SearchBox';
+import Scrollbar from "./Scrollbar";
 import './App.css'
 
 
@@ -11,10 +12,15 @@ class App extends Component {
         super();
         this.state = 
         {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+componentDidMount (){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+    .then (users => this.setState({robots:users}))
+}
 
 onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
@@ -25,15 +31,22 @@ onSearchChange = (event) => {
         const filteredRobots = this.state.robots.filter (robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-            <h1 className="f1"> RoboFriends</h1>
-            <SearchBox searchChange = {this.onSearchChange}/>
-            <CardsList robots={filteredRobots}/>
-            </div>
-        )
-    }
-    
+        if (this.state.robots.length===0)
+        {
+            return <h1 className="f1"> loading, please wait</h1>
+        }
+        else {
+            return (
+                <div className="tc">
+                <h1 className="f1"> RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <Scrollbar>
+                <CardsList robots={filteredRobots}/>
+                </Scrollbar>
+                </div>
+            )
+        }        
+    }    
 }
 
 export default App;
